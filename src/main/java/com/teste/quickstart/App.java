9f3527f54979226944853376661a8df2fde76ec7
tnsousa <thiagonogueirasousa@gmail.com>
2018-01-20 13:12:14 -0200

    Subindo para o meu amor

--- src/main/java/com/teste/quickstart/App.java
+++ src/main/java/com/teste/quickstart/App.java
@@ -1,11 +1,13 @@
 package com.teste.quickstart;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
